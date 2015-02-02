0f0e17294ccf286fa2fb764d3f17e34156c5d39d
yinyanfr <i@yinyan.fr>
2015-02-02 09:38:41 +0100

    Update init.py

--- init.py
+++ init.py
@@ -1,1 +1,52 @@
-print("hello happy")
+# Info224 TD3 4 Yin Yan
+
+# Exo 1
+def supprime_espace_aux_extremites(chaine):
+    res=""
+    i=0
+    z=len(chaine)-1
+    while chaine[i] == " ": # supprimer des espaces au début
+        i = i+1
+    while chaine[z] == " ": # supprimer des espaces à la fin
+        z = z-1
+    res = chaine[i:z+1]
+    return res
+
+# Exo 3
+def exo3_main():
+    t = input("tapez des flottants ")
+    return exo3_output(t)
+
+def exo3_output(t):
+    element=""
+    table=[]
+    for i in range(len(t)):
+        if t[i] != ";" and t[i] != ",":
+            element = element + t[i]
+        elif t[i] == ",":
+            element = element + "."
+        else:
+            table = table + [ float(supprime_espace_aux_extremites(element)) ]
+            element = ""
+    table = table + [ float(supprime_espace_aux_extremites(element)) ]
+    return table
+
+def exo3_output2(t):
+    
+    return
+
+# Palindrome
+# Q1
+def renverse(c):
+    res=""
+    for i in range(len(c)-1,-1,-1):
+        res = res + c[i]
+    return res
+
+# Q2
+def est(c):
+    t=True
+    for i in range(len(c)):
+        if c[i]!=c[-i-1]:
+            t=False
+    return t
