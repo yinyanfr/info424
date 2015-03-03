2d3cb7153f9c5be506157760280cf235ac7a275d
Changhui Shi E sfa l2-sts-stic <shic@localhost>
2015-03-03 16:32:15 +0100

    fuck you

---
+++ type_de_image.py
@@ -0,0 +1,15 @@
+import imghdr
+import imghdr
+
+IMG_PATH = None
+
+def get_ext(path):
+    """retourne une description de type de l'image"""
+    if os.path.exists(path):
+        return imghdr.what(path)
+    else:
+        print("fichier n'existe pas")
+
+def main():
+    img_ext = get_ext(IMG_PATH)
+    print("l'image:[{}], et type est :[{}]".format(IMG_PATH,img_ext))
