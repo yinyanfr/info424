4407363ae6882440581a993caba9feb38c8f1600
shichanghui <shichanghui121@hotmail.com>
2015-03-25 13:48:33 +0100

    Update argu_commande_line.py

--- argu_commande_line.py
+++ argu_commande_line.py
@@ -4,7 +4,15 @@ for i in sys.argv:
     if i = "cercle":
         #appelle la function de crée image et mettre des argument
         #de la liste sys.argv dedans
-        cercle(sys.argv[i+1],sys.argv[i+2],sys.argv[i+3])
+        cercle(sys.argv[i+1],sys.argv[i+2],sys.argv[i+3],sys.argv[i+4])
+    if i = "square":
+        square(sys.argv[i+1],sys.argv[i+2],sys.argv[i+3],sys.argv[i+4],sys.argv[i+5])
+    if i = "disque":
+        disque(sys.argv[i+1],sys.argv[i+2],sys.argv[i+3],sys.argv[i+4])
+    if i = "ligne":
+        ligne(sys.argv[i+1],sys.argv[i+2])
+    if i = "polygon":
+        polygon(sys.argv[i+1],sys.argv[i+2])
 
 ## à finire après finir les fonction        
 
